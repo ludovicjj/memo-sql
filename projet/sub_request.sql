569ff7784728e425691e9f5b93899b4d7232cfb7
ludovicjj <jahanlud@gmail.com>
2022-11-21 14:52:31 +0100

    Add some comment and exemple for sub_query

--- projet/sub_request.sql
+++ projet/sub_request.sql
@@ -1,3 +1,7 @@
+-- Utilisation d'une sous requête dans le FROM
+-- Récupérer toutes les data de chaque ligne de la table recipe
+-- et ajoute une colonne "count" provenant d'une sous requête qui content le nombre d'ingrédient par recette
+-- la sous requête dépendre de l'extérieur (requête corrélé), ici recipe.id
 SELECT *, (
     SELECT COUNT(*)
     FROM recipe_ingredient ri
@@ -6,6 +10,15 @@ SELECT *, (
 FROM recipe r
 ;
 
+-- Utilisation d'une sous requête dans le FROM
+SELECT subquery.title
+FROM (
+    SELECT * FROM  recipe
+) AS subquery;
+
+
+-- Utilisation d'une sous requête dans le WHERE
+-- Récupérer toutes les recettes qui n'appartiennent pas à la catégorie Gateau et Dessert
 SELECT *
 FROM recipe r
 WHERE r.id IN (
